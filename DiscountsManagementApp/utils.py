336f15b45ea1629c958076902b5e5d4a1e8faba5
Ngàn Thành Phú <nhocjoon123@gmail.com>
2026-04-10 16:38:06 +0700

    fix: not allow user promotion usage reach 0

--- DiscountsManagementApp/utils.py
+++ DiscountsManagementApp/utils.py
@@ -2,9 +2,9 @@ from DiscountsManagementApp import db
 from DiscountsManagementApp.dao import create_user_promotion_usage
 
 
-def update_availability(user, promotion, user_promotion_usage=None, increment_usage=True):
+def update_availability(user, promotion=None, user_promotion_usage=None, increment_usage=True):
     step = 1 if increment_usage else -1
-    if user_promotion_usage and user_promotion_usage.usage_count + step > 0:
+    if user_promotion_usage and user_promotion_usage.usage_count + step >= 0:
         user_promotion_usage.usage_count += step
     else:
         if increment_usage and promotion:
