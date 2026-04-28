854617dabc1c98d405075dbdc5f6bd2fea1830f3
Ngàn Thành Phú <nhocjoon123@gmail.com>
2026-04-28 14:48:11 +0700

    fix: add validation case of page number input in promotions API (#29)

--- DiscountsManagementApp/index.py
+++ DiscountsManagementApp/index.py
@@ -164,9 +164,9 @@ def register_routes(target_app):
         code = request.args.get('code')
         amount = request.args.get('amount', type=float)
         ptype = request.args.get('ptype')
-        page = request.args.get('page', 1, type=int)
+        page = request.args.get('page', None, type=int)
         
-        if page < 1:
+        if isinstance(page, int) and page < 1:
             return jsonify({'error': 'Số trang không hợp lệ!'}), 400
 
         if amount and amount < 0:
